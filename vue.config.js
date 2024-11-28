8168842c504b921ef37527d4a08a0e3f2f1bed2a
Hy <40989847+010422@users.noreply.github.com>
2024-11-28 21:34:02 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,83 +1,11 @@
-const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader');
-const webpack = require('webpack');
-
-function resolve(dir) {
-  return path.join(__dirname, dir);
-}
-
 module.exports = {
-  productionSourceMap: false,
-  devServer: {
-    disableHostCheck: true,
-    port: process.env.DEV_SERVER_PORT || 8080,
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/',
-        },
-      },
-    },
-  },
-  pwa: {
-    name: 'YesPlayMusic',
-    iconPaths: {
-      favicon32: 'img/icons/favicon-32x32.png',
-    },
-    themeColor: '#ffffff00',
-    manifestOptions: {
-      background_color: '#335eea',
-    },
-  },
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'YesPlayMusic',
-      chunks: ['main', 'chunk-vendors', 'chunk-common', 'index'],
-    },
-  },
-  chainWebpack(config) {
-    config.module.rules.delete('svg');
-    config.module.rule('svg').exclude.add(resolve('src/assets/icons')).end();
+  chainWebpack: config => {
     config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/assets/icons'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]',
-      })
+      .rule('vue')
+      .test(/\.vue$/)
+      .use('vue-loader')
+      .loader('vue-loader')
       .end();
-
-    config.module
-      .rule('napi')
-      .test(/\.node$/)
-      .use('node-loader')
-      .loader('node-loader')
-      .end();
-
-    config.module
-      .rule('webpack4_es_fallback')
-      .test(/\.js$/)
-      .include.add(/node_modules/)
-      .end()
-      .use('esbuild-loader')
-      .loader('esbuild-loader')
-      .options({ target: 'es2015', format: "cjs" })
-      .end();
-
-    config.plugin('chunkPlugin').use(webpack.optimize.LimitChunkCountPlugin, [
-      {
-        maxChunks: 3,
-        minChunkSize: 10000,
-      },
-    ]);
   },
   pluginOptions: {
     electronBuilder: {
@@ -163,32 +91,6 @@ module.exports = {
           perMachine: true,
           deleteAppDataOnUninstall: true,
         },
-      },
-      chainWebpackMainProcess: config => {
-        config.plugin('define').tap(args => {
-          args[0]['IS_ELECTRON'] = true;
-          return args;
-        });
-        config.resolve.alias.set(
-          'jsbi',
-          path.join(__dirname, 'node_modules/jsbi/dist/jsbi-cjs.js')
-        );
-
-        config.module
-          .rule('webpack4_es_fallback')
-          .test(/\.js$/)
-          .include.add(/node_modules/)
-          .end()
-          .use('esbuild-loader')
-          .loader('esbuild-loader')
-          .options({ target: 'es2015', format: "cjs" })
-          .end();
-      },
-      chainWebpackRendererProcess: config => {
-        config.plugin('define').tap(args => {
-          args[0]['IS_ELECTRON'] = true;
-          return args;
-        });
       },
     },
   },
