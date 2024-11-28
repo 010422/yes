99df4445aef3aa3be0ad225c2d1a26e1143011bc
Hy <40989847+010422@users.noreply.github.com>
2024-11-28 19:47:27 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,80 @@
-const path = require('path');
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  target: 'electron-main',
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+  productionSourceMap: false,
+  devServer: {
+    disableHostCheck: true,
+    port: process.env.DEV_SERVER_PORT || 8080,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/',
+        },
+      },
+    },
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
+  chainWebpack(config) {
+    config.module.rules.delete('svg');
+    config.module.rule('svg').exclude.add(path.resolve(__dirname, 'src/assets/icons')).end();
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(path.resolve(__dirname, 'src/assets/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end();
+    config.module
+      .rule('napi')
+      .test(/\.node$/)
+      .use('node-loader')
+      .loader('node-loader')
+      .end();
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.IS_ELECTRON': JSON.stringify(true)
-    })
-  ]
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      externals: ['@unblockneteasemusic/rust-napi'],
+      builderOptions: {
+        productName: 'YesPlayMusic',
+        copyright: 'Copyright © YesPlayMusic',
+        asar: true,
+        publish: [
+          {
+            provider: 'github',
+            owner: 'qier222',
+            repo: 'YesPlayMusic',
+            vPrefixedTagName: true,
+            releaseType: 'draft',
+          },
+        ],
+        directories: {
+          output: 'dist_electron',
+        },
+        mac: {
+          target: ['dmg'],
+          artifactName: '${productName}-${os}-${version}-${arch}.${ext}',
+          category: 'public.app-category.music',
+          darkModeSupport: true,
+        },
+        win: {
+          target: ['portable', 'nsis'],
+          publisherName: 'YesPlayMusic',
+          icon: 'build/icons/icon.ico',
+        },
+        linux: {
+          target: ['AppImage', 'tar.gz', 'deb', 'rpm'],
+        },
+        dmg: {
+          icon: 'build/icons/icon.icns',
+        },
+      },
+    },
+  },
 };
